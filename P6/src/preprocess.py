c034601c50330837b3c2f1c47bbe2ad672bc88fd
peilin16 <105468929+peilin16@users.noreply.github.com>
2025-02-20 19:11:22 -0800

    Add files via upload

--- P6/src/preprocess.py
+++ P6/src/preprocess.py
@@ -1,4 +1,5 @@
 from keras.utils import image_dataset_from_directory
+#from tensorflow.python.keras.applications.mobilenet_v2 import preprocess_input 
 from config import train_directory, test_directory, image_size, batch_size, validation_split
 import os
 def _split_data(train_directory, test_directory, batch_size, validation_split):
@@ -14,6 +15,8 @@ def _split_data(train_directory, test_directory, batch_size, validation_split):
         subset="both",
         seed=47
     )
+
+
     print('test dataset:')
     test_dataset = image_dataset_from_directory(
         test_directory,
@@ -35,5 +38,33 @@ def get_transfer_datasets():
     # Your code replaces this by loading the dataset
     # you can use image_dataset_from_directory, similar to how the _split_data function is using it
     train_dataset, validation_dataset, test_dataset = None, None, None
-    # ...
+    train_dataset = image_dataset_from_directory(
+        'transfer_train',
+        label_mode='categorical',
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=(150,150),
+        validation_split=validation_split,
+        subset="training",
+        seed=47
+    )
+    validation_dataset = image_dataset_from_directory(
+        'transfer_train',
+        label_mode='categorical',
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=(150,150),
+        validation_split=validation_split,
+        subset="validation",
+        seed=47
+    )
+    test_dataset = image_dataset_from_directory(
+        'transfer_test',
+        label_mode='categorical',
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=(150,150),
+        shuffle=False
+    )
+    
     return train_dataset, validation_dataset, test_dataset
